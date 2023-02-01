e7981f05da84ce80e82df0a0708c34256a127983
AleksandraDrzewiecka <111142458+AleksandraDrzewiecka@users.noreply.github.com>
2023-02-01 11:23:56 +0100

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -13,7 +13,6 @@ const toTopBtn = document.getElementById('to-top');
 
 let pageNo = 1;
 
-loadMoreBtn.style.display = 'none';
 toTopBtn.style.display = 'none';
 
 const lightbox = new SimpleLightbox('.gallery a');
